ebf3b9770fb72bb5ec9ca94acdecf2716356d895
ecal590 <ecal590@gmail.com>
2017-05-27 17:34:25 -0500

    The main three screens are laid out.

---
+++ app/src/main/java/com/example/ebonycalloway/fridgefriend/FoodLibrary.java
@@ -0,0 +1,13 @@
+package com.example.ebonycalloway.fridgefriend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodLibrary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_library);
+    }
+}
--- app/src/main/java/com/example/ebonycalloway/fridgefriend/MainActivity.java
+++ app/src/main/java/com/example/ebonycalloway/fridgefriend/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.ebonycalloway.fridgefriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton settingB = (ImageButton) findViewById(R.id.settingsButton);
+        Button addB = (Button) findViewById(R.id.addItemButton);
+        Button viewLibraryB = (Button) findViewById(R.id.viewLibraryButton);
+
+        settingB.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent(MainActivity.this, Settings.class);
+                startActivity(i);
+            }
+
+        });
+
+        addB.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent(MainActivity.this, AddItem.class);
+                startActivity(i);
+            }
+
+        });
+
+        viewLibraryB.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent(MainActivity.this, FoodLibrary.class);
+                startActivity(i);
+            }
+
+        });
+
+
+
     }
 }
